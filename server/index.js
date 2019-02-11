b2cb7dc2ade405a15b5ff37250aa8ef8700212d5
Nathan Hill <nateohill@gmail.com>
2019-02-10 22:28:41 -0800

    Correct which file index.js was serving up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require('exress');
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(`${__dirname}/client/dist`));
+app.use(express.static(`${__dirname}/../client/dist`));
 
 app.listen(PORT, () => {
   console.log(`App is listening on: ${PORT}`);
