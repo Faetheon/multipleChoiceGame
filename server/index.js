1af8001bef16ecc8982f8ca546100a44765d4174
Nathan Hill <nateohill@gmail.com>
2019-02-12 11:14:14 -0800

    Added a easter egg route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/../client/dist`));
 
+app.get('/easterEgg', (req, res) => {
+  res.send(`<head><title>Wow, this worked?</title></head><body><div>He dies at the end :P</div></body>`);
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on: ${PORT}`);
 });
