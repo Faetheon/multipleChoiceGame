4b691580c054d06c3b4bf799b7018d08802541ff
Nathan Hill <nateohill@gmail.com>
2019-02-12 13:22:29 -0800

    Fixed a typo and added temporary lorem ipsum text to the WIP choices

--- client/src/stories/AWalkInTheWoods.js
+++ client/src/stories/AWalkInTheWoods.js
@@ -9,20 +9,20 @@ export default {
   choiceOne: {
     choices: {
       North: `
-        Looking towards the Nort you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
+        Looking towards the North you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
         After glancing around you see a long branch on the other end
       `,
       East: `
-        WIP East
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis egestas nisi, a viverra nulla. Nunc feugiat vestibulum elit, non pulvinar enim dapibus ut. Cras in tellus sit amet tellus ullamcorper consectetur non nec lectus. Morbi ultrices arcu ac posuere vestibulum. Nulla quis lacus id quam consectetur finibus at id leo. Ut arcu risus, varius sed urna sed, convallis commodo mi. Duis commodo placerat magna nec tempus. Nulla facilisi. Mauris posuere semper justo, non rutrum justo iaculis ut. Integer pharetra diam sed purus gravida, vel blandit velit aliquet. Curabitur eu felis sed purus interdum pharetra. Donec quis dui facilisis, tristique erat at, vehicula nunc.
       `,
       South: `
-        WIP South
+        Suspendisse potenti. Fusce sed mauris urna. Suspendisse suscipit, quam sed posuere congue, lorem mi vulputate mi, non blandit leo nulla vitae risus. Vivamus cursus porta nunc, vitae malesuada nulla aliquet nec. Ut a iaculis tellus, sit amet ullamcorper massa. Quisque egestas, velit eget feugiat convallis, velit erat eleifend urna, id sodales magna lorem eu metus. Cras risus nunc, semper vel volutpat eget, pulvinar vitae libero. Mauris bibendum, ex ac egestas vestibulum, metus tellus ultrices velit, vel porta lectus tellus vel mauris.
       `,
       West: `
-        WIP West
+        Integer maximus erat id mi condimentum congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque id nunc vitae leo porta elementum. Nulla sed risus lectus. Nunc gravida tellus ac dolor facilisis eleifend et eget dui. Morbi accumsan pellentesque elit eget pharetra. Morbi consequat urna et neque ultricies vehicula. Aliquam semper eget elit condimentum pulvinar. Sed ut risus purus. Vivamus nec diam eros. Cras eu felis id ipsum porttitor ultricies eu ac justo. Duis ac sapien ut justo sagittis aliquet eu at neque. Cras commodo malesuada gravida.
       `,
       backpack: `
-        WIP backpack
+        In et egestas neque. Cras ullamcorper elit at dui lacinia scelerisque. Curabitur in dignissim nunc, ac molestie dui. Nunc dictum vel ipsum ultricies dignissim. Ut elit eros, tristique in aliquam accumsan, placerat ut velit. Aenean fringilla pharetra pulvinar. Nulla iaculis lacus at nisl posuere, luctus fringilla magna finibus. Etiam laoreet interdum tortor, id malesuada sapien mollis vitae. Ut interdum nunc eget blandit vehicula. Ut ac vehicula ipsum. Praesent faucibus risus sit amet risus imperdiet vehicula. Quisque pulvinar consectetur pulvinar.
       `,
       lookAround: `
         You look around and see a path heading towards the East, 
