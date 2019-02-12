df0c977d21dbb53f083d12df6a41c0b1a2e90167
Nathan Hill <nateohill@gmail.com>
2019-02-12 10:47:00 -0800

    Added two choices and a prelude to the first story Made the story in a different file as a JavaScript object

---
+++ client/src/stories/AWalkInTheWoods.js
@@ -0,0 +1,132 @@
+export default {
+  name: 'A Walk In The Woods',
+  prelude: 
+  `
+  You've woken up underneath a tall pine in the middle of some woods, having no Idea where you are you look around and see a path heading towards the East, 
+  some light shrubbery to the North, a backpack stuck in the branches of the pine you woke up under, to the West and South there seems to be impassable thick briars.
+  `
+  ,
+  choiceOne: {
+    choices: {
+      North: `
+        Looking towards the Nort you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
+        After glancing around you see a long branch on the other end
+      `,
+      East: `
+        WIP East
+      `,
+      South: `
+        WIP South
+      `,
+      West: `
+        WIP West
+      `,
+      backpack: `
+        WIP backpack
+      `,
+      lookAround: `
+        You look around and see a path heading towards the East, 
+        some shrubs to the North, a backpack stuck in the branches of the pine you woke up under, to the West and South there seems to be thick briars with thorns galore.
+      `
+    }
+  },
+  northChoice: {
+    choices: {
+      North: `
+        Looking towards the Nort you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
+        After glancing around you see a long branch on the other end
+      `,
+      East: `
+        WIP East
+      `,
+      South: `
+        WIP South
+      `,
+      West: `
+        WIP West
+      `,
+      backpack: `
+        WIP backpack
+      `,
+      lookAround: `
+        You look around and see a path heading towards the East, 
+        some shrubs to the North, a backpack stuck in the branches of the pine you woke up under, to the West and South there seems to be thick briars with thorns galore.
+      `
+    }
+  },
+  southChoice: {
+    choices: {
+      North: `
+        Looking towards the Nort you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
+        After glancing around you see a long branch on the other end
+      `,
+      East: `
+        WIP East
+      `,
+      South: `
+        WIP South
+      `,
+      West: `
+        WIP West
+      `,
+      backpack: `
+        WIP backpack
+      `,
+      lookAround: `
+        You look around and see a path heading towards the East, 
+        some shrubs to the North, a backpack stuck in the branches of the pine you woke up under, to the West and South there seems to be thick briars with thorns galore.
+      `
+    }
+  },
+  eastChoice: {
+    choices: {
+      North: `
+        Looking towards the Nort you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
+        After glancing around you see a long branch on the other end
+      `,
+      East: `
+        WIP East
+      `,
+      South: `
+        WIP South
+      `,
+      West: `
+        WIP West
+      `,
+      backpack: `
+        WIP backpack
+      `,
+      lookAround: `
+        You look around and see a path heading towards the East, 
+        some shrubs to the North, a backpack stuck in the branches of the pine you woke up under, to the West and South there seems to be thick briars with thorns galore.
+      `
+    }
+  },
+  westChoice: {
+    choices: {
+      North: `
+        Looking towards the Nort you start heading through the forest being sure to keep your footing while moving thick vines out of the way. You arrive in a small clearing.
+        After glancing around you see a long branch on the other end
+      `,
+      East: `
+        WIP East
+      `,
+      South: `
+        WIP South
+      `,
+      West: `
+        WIP West
+      `,
+      backpack: `
+        WIP backpack
+      `,
+      lookAround: `
+        You look around and see a path heading towards the East, 
+        some shrubs to the North, a backpack stuck in the branches of the pine you woke up under, to the West and South there seems to be thick briars with thorns galore.
+      `
+    }
+  },
+  backpackChoice: {
+
+  }
+}
