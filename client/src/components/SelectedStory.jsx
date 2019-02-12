1521871df206bb5139bf1022b24d27ba87c83b32
Nathan Hill <nateohill@gmail.com>
2019-02-12 13:21:55 -0800

    Changed the format of the options available to the reader

--- client/src/components/SelectedStory.jsx
+++ client/src/components/SelectedStory.jsx
@@ -2,13 +2,14 @@ import React, {useState} from 'react';
 
 function SelectedStory({storyData}) {
   const [storyChoiceKeys, updateStoryChoiceKeys] = useState(Object.keys(storyData.choiceOne.choices))
-  console.log(storyChoiceKeys)
   return (
     <div className='selected-story'>
-      {storyData ? storyData.prelude : null}
+      <div className='prelude'>
+        {storyData ? storyData.prelude : null}
+      </div>
       {
         storyChoiceKeys.map((elem, i) => (
-          <div className={`choice ${i + 1}`} key={i}>{`${i + 1}. ${storyData.choiceOne.choices[elem]}`}</div>
+          <div className={`choice ${i + 1}`} key={i}>{storyData.choiceOne.choices[elem]}</div>
         ))
       }
     </div>
