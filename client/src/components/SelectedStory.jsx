fb893381770f39c050fdc7a334dee5fea38b06f3
Nathan Hill <nateohill@gmail.com>
2019-02-12 11:17:54 -0800

    Added a hook to Selected story component The added hook allows for the mapping over of the keys from the story the user selected while also helping map out the choices from the story

--- client/src/components/SelectedStory.jsx
+++ client/src/components/SelectedStory.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function SelectedStory({storyData}) {
+  const [storyChoiceKeys, updateStoryChoiceKeys] = useState(Object.keys(storyData.choiceOne.choices))
+  console.log(storyChoiceKeys)
   return (
-    <div>
+    <div className='selected-story'>
       {storyData ? storyData.prelude : null}
+      {
+        storyChoiceKeys.map((elem, i) => (
+          <div className={`choice ${i + 1}`} key={i}>{`${i + 1}. ${storyData.choiceOne.choices[elem]}`}</div>
+        ))
+      }
     </div>
   );
 }
